a9299a28a0313c97d9f039bf40cc7b9029dd66c0
jeongwonlee-dev <jeongwonlee-dev-git@users.noreply.github.com>
2020-11-25 17:47:34 +0900

    no added.

--- app/src/main/java/com/example/lighting/fragment/onoff.java
+++ app/src/main/java/com/example/lighting/fragment/onoff.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -19,46 +20,34 @@ public class onoff extends Fragment {
     Button btn_on;
     Button btn_off;
 
-    public byte[] sendByte = new byte[5];
+    //public byte[] sendByte = new byte[5];
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_onoff, container, false);
 
+        btn_on = (Button) v.findViewById(R.id.button_on);
+        btn_off = (Button) v.findViewById(R.id.button_off);
 
-
-
-
+        btn_on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_onoff_to_ledmenu);
+            }
+        });
         return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btn_on = (Button)view.findViewById(R.id.button_on);
-        btn_off = (Button)view.findViewById(R.id.button_off);
-
-        //on버튼 클릭 시 led메뉴들 화면으로 전환
-        view.findViewById(R.id.button_on).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_onoff_to_ledmenu);
-            }
-        });
-
-        //off버튼 클릭 시 led 꺼짐 액션 수행
-        btn_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*
-            //아래 두줄 꺼짐 액션 만들어서 수정하기
-                NavHostFragment.findNavController(onoff.this)
-                        .navigate(R.id.action_onoff_to_ledmenu);
-                 */
-            }
-
-        });
     }
 }
